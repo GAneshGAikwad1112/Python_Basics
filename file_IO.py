6fd52b8c0c22e88e965bf257d3809e07f65abcfd
ganeshgaikwad1112 <gaikwadg1112@gmail.com>
2024-04-13 10:54:46 +0530

    Adding input output file in python

--- file_IO.py
+++ file_IO.py
@@ -5,6 +5,28 @@
         
         Types of all files:
             1. Text files: .txt, .docs, .log etc
-            2. Binary Files: .mp4, .mov, .png, .jpeg etc
+            2. Binary Files: .mp4, .mov, .png, .jpeg etc.
+            
+        syntax: 
+                f = open("file_name","mode")
             
 '''
+
+
+f = open("domo.txt","r")
+data = f.read()
+print(data)
+print(type(data))
+f.close()
+
+'''
+'r' - open for reading(default)
+'w' - open for writing, truncating the file first
+'x' - create a new file and open it for writing
+'a' - open for writing, appending to the end of the file if it exists 
+'b' - binary mode
+'t' - text mode(default)
+'+' - open a disk file for updating(reading and writing)
+
+'''
+
